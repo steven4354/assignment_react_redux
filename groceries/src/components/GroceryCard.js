0679a926cd019a99b99a6bbb480f5b8aa47ca9bb
Steven Li <steven4354@gmail.com>
2018-01-02 13:32:28 -0800

    working on the new item form

--- groceries/src/components/GroceryCard.js
+++ groceries/src/components/GroceryCard.js
@@ -1,28 +1,37 @@
-import React, { PropTypes } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-const GroceryCard = ({ item }) => {
-  const { name, id, category, available, description } = item;
+const GroceryCard = ({item}) => {
+  const {name, id, category, available, description} = item;
 
   // Using Bootstrap 4 card classes
   return (
-    <div className="PuppyCard card" style={{ maxWidth: "320px" }}>
-      <img
-        className="card-img-top img-fluid"
-        src={avatar ? avatar : "http://placehold.it/320x320"}
-        alt="puppy avatar"
-      />
+    <div
+      className="GroceryCard card"
+      style={{
+        maxWidth: "320px",
+        backgroundColor: "gray",
+        padding: "6px",
+        marginBottom: "10px"
+      }}
+    >
       <div className="card-block">
         <h4>{name}</h4>
-        <p>{breed}</p>
-        <AdoptButton available={available} onAdoptClick={onAdoptClick} />
+        <p> id: {id} </p>
+        <p> category: {category}</p>
+        <p> purchased: {!available}</p>
+        <p> description: {description}</p>
       </div>
     </div>
   );
 };
 
-PuppyCard.propTypes = {
-  puppy: PropTypes.object.isRequired,
-  onAdoptClick: PropTypes.func.isRequired
+/*
+<AdoptButton available={available} onAdoptClick={onAdoptClick} />
+*/
+
+GroceryCard.propTypes = {
+  item: PropTypes.object.isRequired
 };
 
-export default PuppyCard;
+export default GroceryCard;
--- groceries/src/components/GroceryList.js
+++ groceries/src/components/GroceryList.js
@@ -1,12 +1,47 @@
-import React from "react";
-
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-const GroceryList = ({ items, itemFilters }) => {
+//custom components
+import GroceryCard from "./GroceryCard";
+
+class GroceryList extends Component {
   // Using Bootstrap 4 card layout
-  console.log(items);
-  return <div className="GroceryList container">To Be Made</div>;
-};
+  constructor(props) {
+    //props: {items, itemFilters, createItem}
+    super();
+    this.state = {
+      showingForm: false
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.showForm = this.showForm.bind(this);
+  }
+
+  showForm(e) {
+    e.preventDefault;
+    this.setState({showingForm: true});
+  }
+
+  render() {
+    return (
+      <div className="GroceryList container">
+        {this.props.items.map(obj => {
+          return <GroceryCard item={obj} key={obj.id} />;
+        })}
+        <button className={`btn btn-danger btn-lg`} onClick={this.showForm}>
+          Add new grocery item
+        </button>
+        {this.state.showingForm ? (
+          <div>
+            <form />
+          </div>
+        ) : (
+          <div />
+        )}
+      </div>
+    );
+  }
+}
 
 GroceryList.propTypes = {
   items: PropTypes.array.isRequired,
