970f583a5cca08bf0e46304a3e626c6ecf0205e9
Lakshmi <lakshmi.nishthala@gmail.com>
2018-01-02 13:00:28 -0800

    Building grocery card

--- groceries/src/components/GroceryCard.js
+++ groceries/src/components/GroceryCard.js
@@ -5,11 +5,11 @@ const GroceryCard = ({ item }) => {
 
   // Using Bootstrap 4 card classes
   return (
-    <div className="PuppyCard card" style={{ maxWidth: "320px" }}>
+    <div className="GroceryCard card" style={{ maxWidth: "320px" }}>
       <img
         className="card-img-top img-fluid"
         src={avatar ? avatar : "http://placehold.it/320x320"}
-        alt="puppy avatar"
+        alt="grocery avatar"
       />
       <div className="card-block">
         <h4>{name}</h4>
